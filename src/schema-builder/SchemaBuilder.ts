ce84203f10168c650f0e35bba1aa8f469908e673
Umed Khudoiberdiev <zarrhost@gmail.com>
2016-08-28 20:27:38 +0500

    removed old methods from schema builder

--- src/schema-builder/SchemaBuilder.ts
+++ src/schema-builder/SchemaBuilder.ts
@@ -5,15 +5,6 @@ import {IndexMetadata} from "../metadata/IndexMetadata";
 import {TableSchema} from "../schema-creator/TableSchema";
 import {ColumnSchema} from "../schema-creator/ColumnSchema";
 
-export interface DatabaseColumnProperties {
-    name: string;
-    type: string;
-    nullable: boolean;
-    generated: boolean;
-    hasPrimaryKey: boolean;
-    comment: string|undefined;
-}
-
 /**
  * todo: make internal too (need to refactor driver).
  */
@@ -30,18 +21,5 @@ export abstract class SchemaBuilder {
     abstract dropIndex(tableName: string, indexName: string): Promise<void>;
     abstract createUniqueKey(tableName: string, columnName: string, keyName: string): Promise<void>;
     abstract normalizeType(column: ColumnMetadata): any;
-
-    /**
-     * @deprecated
-     */
-    abstract changeColumnQuery(tableName: string, oldColumn: DatabaseColumnProperties, newColumn: ColumnMetadata): Promise<void>;
-    abstract getTableUniqueKeysQuery(tableName: string): Promise<string[]>;
-    abstract getTableIndicesQuery(tableName: string): Promise<{ key: string, sequence: number, column: string }[]>;
-    abstract getPrimaryConstraintName(tableName: string): Promise<string>;
-    abstract renameColumnQuery(tableName: string, oldColumn: DatabaseColumnProperties, newColumnName: ColumnMetadata): Promise<void>;
-    abstract getTableForeignQuery(tableName: string): Promise<string[]>;
-    abstract getChangedColumns(tableName: string, columns: ColumnMetadata[]): Promise<DatabaseColumnProperties[]>;
-    abstract checkIfTableExist(tableName: string): Promise<boolean>;
-    abstract getTableColumns(tableName: string): Promise<DatabaseColumnProperties[]>;
 
 }
