0e920c0ca1aae50f8596a86d882c845382bad4df
Umed Khudoiberdiev <zarrhost@gmail.com>
2016-09-08 19:41:12 +0500

    fixed failing test issue

--- sample/sample1-simple-entity/entity/Post.ts
+++ sample/sample1-simple-entity/entity/Post.ts
@@ -3,14 +3,8 @@ import {PrimaryColumn, Column, Table} from "../../../src/index";
 @Table("sample1_post")
 export class Post {
 
-    @PrimaryColumn("int")
-    id: number = 2;
-
-    @PrimaryColumn("int")
-    id2: number = 3;
-
-    @PrimaryColumn("int")
-    id3: number = 1;
+    @PrimaryColumn("int", { generated: true })
+    id: number;
 
     @Column()
     title: string;
