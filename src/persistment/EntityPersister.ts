6fb2e8198c10ea41d7e7e3b5126f8ab0098c6ab9
Umed Khudoiberdiev <zarrhost@gmail.com>
2016-09-26 10:20:38 +0500

    refactroed persistment a bit. stopping for now.

--- src/persistment/EntityPersister.ts
+++ src/persistment/EntityPersister.ts
@@ -30,26 +30,30 @@ export class EntityPersister<Entity extends ObjectLiteral> {
      * Persists given entity in the database.
      */
     async persist(entity: Entity): Promise<Entity> {
-        const allPersistedEntities = await this.flattenEntityRelationTree(entity, this.metadata);
-        const plainObjectToDatabaseEntityTransformer = new PlainObjectToDatabaseEntityTransformer();
+        const allNewEntities = await this.flattenEntityRelationTree(entity, this.metadata);
+        const persistedEntity = allNewEntities.find(operatedEntity => operatedEntity.entity === entity);
+        if (!persistedEntity)
+            throw new Error(`Internal error. Persisted entity was not found in the list of prepared operated entities`);
 
-        let dbEntity: OperateEntity|undefined, entityWithIds: OperateEntity[] = [];
+        let dbEntity: OperateEntity|undefined, allDbInNewEntities: OperateEntity[] = [];
+
+        // if entity has an id then check
         if (this.hasId(entity)) {
             const queryBuilder = new QueryBuilder<Entity>(this.connection, this.queryRunner)
                 .select(this.metadata.table.name)
                 .from(this.metadata.target, this.metadata.table.name);
+            const plainObjectToDatabaseEntityTransformer = new PlainObjectToDatabaseEntityTransformer();
             const loadedDbEntity = await plainObjectToDatabaseEntityTransformer.transform(entity, this.metadata, queryBuilder);
             if (loadedDbEntity) {
                 dbEntity = new OperateEntity(this.metadata, loadedDbEntity);
-                entityWithIds = await this.flattenEntityRelationTree(loadedDbEntity, this.metadata);
+                allDbInNewEntities = await this.flattenEntityRelationTree(loadedDbEntity, this.metadata);
             }
         }
 
         // need to find db entities that were not loaded by initialize method
-        const allDbEntities = await this.findNotLoadedIds(allPersistedEntities, entityWithIds);
-        const persistedEntity = new OperateEntity(this.metadata, entity);
+        const allDbEntities = await this.findNotLoadedIds(allNewEntities, allDbInNewEntities);
         const entityPersistOperationBuilder = new EntityPersistOperationBuilder(this.connection.entityMetadatas);
-        const persistOperation = entityPersistOperationBuilder.buildFullPersistment(dbEntity, persistedEntity, allDbEntities, allPersistedEntities);
+        const persistOperation = entityPersistOperationBuilder.buildFullPersistment(dbEntity, persistedEntity, allDbEntities, allNewEntities);
 
         const persistOperationExecutor = new PersistOperationExecutor(this.connection.driver, this.connection.entityMetadatas, this.connection.broadcaster, this.queryRunner); // todo: better to pass connection?
         await persistOperationExecutor.executePersistOperation(persistOperation);
