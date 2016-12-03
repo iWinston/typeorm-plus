3c5c5f672ffeb76c5ce7d570e6cddd0d427a2c26
Umed Khudoiberdiev <zarrhost@gmail.com>
2016-12-03 12:37:34 +0500

    #62 and #61 added missing browser platform tools methods

--- src/platform/BrowserPlatformTools.ts
+++ src/platform/BrowserPlatformTools.ts
@@ -20,4 +20,39 @@ export class BrowserPlatformTools {
         throw new Error(`This option/function is not supported in the browser environment. Failed operation: require("${name}").`);
     }
 
+    /**
+     * Normalizes given path. Does "path.normalize".
+     */
+    static pathNormilize(pathStr: string): string {
+        throw new Error(`This option/function is not supported in the browser environment. Failed operation: path.normalize("${pathStr}").`);
+    }
+
+    /**
+     * Gets file extension. Does "path.extname".
+     */
+    static pathExtname(pathStr: string): string {
+        throw new Error(`This option/function is not supported in the browser environment. Failed operation: path.extname("${pathStr}").`);
+    }
+
+    /**
+     * Resolved given path. Does "path.resolve".
+     */
+    static pathResolve(pathStr: string): string {
+        throw new Error(`This option/function is not supported in the browser environment. Failed operation: path.resolve("${pathStr}").`);
+    }
+
+    /**
+     * Synchronously checks if file exist. Does "fs.existsSync".
+     */
+    static fileExist(pathStr: string): boolean {
+        throw new Error(`This option/function is not supported in the browser environment. Failed operation: fs.existsSync("${pathStr}").`);
+    }
+
+    /**
+     * Gets environment variable.
+     */
+    static getEnvVariable(name: string): any {
+        throw new Error(`This option/function is not supported in the browser environment. Failed operation: process.env["${name}"].`);
+    }
+
 }
