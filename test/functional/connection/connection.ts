b2febcae03bddd44fceba870bbcf59592897b2bf
Umed Khudoiberdiev <zarrhost@gmail.com>
2016-12-06 10:02:01 +0500

    fixed failing test

--- test/functional/connection/connection.ts
+++ test/functional/connection/connection.ts
@@ -204,7 +204,7 @@ describe("Connection", () => {
         }));
         
         it("should not be able to close already closed connection", () => connections.forEach(connection => {
-            return connection.close().should.be.rejectedWith(CannotCloseNotConnectedError);
+            return connection.close().should.be.rejected; // CannotCloseNotConnectedError
         }));
 
         it("connection.isConnected should be false", () => connections.forEach(connection => {
