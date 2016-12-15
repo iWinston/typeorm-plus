01f7fb7c47105b24c230e2cf67b3d7cce99a87d3
Dave <st0724@gmail.com>
2016-12-15 03:58:39 -0500

    added extra test case for persisting jsonb

--- test/functional/json/entity/Record.ts
+++ test/functional/json/entity/Record.ts
@@ -2,16 +2,19 @@ import { Table } from "../../../../src/decorator/tables/Table";
 import { PrimaryGeneratedColumn } from "../../../../src/decorator/columns/PrimaryGeneratedColumn";
 import { Column } from "../../../../src/decorator/columns/Column";
 
+/**
+ * For testing Postgres jsonb
+ */
 @Table()
 export class Record {
 
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({ type: "json" })
+    @Column({ type: "json", nullable: true })
     config: any;
 
-    @Column({ type: "jsonb" })
+    @Column({ type: "jsonb", nullable: true })
     data: any;
 
 }
--- test/functional/json/jsonb.ts
+++ test/functional/json/jsonb.ts
@@ -6,7 +6,7 @@ import { createTestingConnections, reloadTestingDatabases, closeTestingConnectio
 import { DataTypeNotSupportedByDriverError } from "../../../src/driver/error/DataTypeNotSupportedByDriverError";
 import { PostgresDriver } from "../../../src/driver/postgres/PostgresDriver";
 
-describe("json type", () => {
+describe("jsonb type", () => {
 
     let connections: Connection[];
     before(async () => connections = await createTestingConnections({
@@ -15,7 +15,7 @@ describe("json type", () => {
     // beforeEach(() => reloadTestingDatabases(connections));
     after(() => closeTestingConnections(connections));
 
-    it("should deal with Postgres' jsonb type correctly", () => Promise.all(connections.map(async connection => {
+    it("should make correct schema with Postgres' jsonb type", () => Promise.all(connections.map(async connection => {
         if (connection.driver instanceof PostgresDriver) {
             await connection.syncSchema(true);
             const queryRunner = await connection.driver.createQueryRunner();
@@ -26,6 +26,19 @@ describe("json type", () => {
             expect(schema.columns.find(columnSchema => columnSchema.name === "data" && columnSchema.type === "jsonb")).to.be.not.undefined;
         } else {
             expect(connection.syncSchema(true)).to.eventually.throw(DataTypeNotSupportedByDriverError);
+        }
+    })));
+
+    it("should persist jsonb correctly", () => Promise.all(connections.map(async connection => {
+        if (connection.driver instanceof PostgresDriver) {
+            await connection.syncSchema(true);
+            let recordRepo = connection.getRepository(Record);
+            let record = new Record();
+            record.data = { foo: "bar" };
+            let persistedRecord = await recordRepo.persist(record);
+            let foundRecord = await recordRepo.findOneById(persistedRecord.id);
+            expect(foundRecord).to.be.not.undefined;
+            expect(foundRecord!.data.foo).to.eq("bar");
         }
     })));
 
