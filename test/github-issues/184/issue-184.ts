2c9686c1348dd0b74b108c33d253c9f920ba98f4
Danijel <dan.baternik@gmail.com>
2017-01-15 16:36:38 +1000

    Added test case for issue 184

--- test/github-issues/184/issue-184.ts
+++ test/github-issues/184/issue-184.ts
@@ -3,6 +3,8 @@ import {createTestingConnections, closeTestingConnections, reloadTestingDatabase
 import {Connection} from "../../../src/connection/Connection";
 import {Employee} from "./entity/Employee";
 import {Homesitter} from "./entity/Homesitter";
+import {Person} from "./entity/Person";
+import {expect} from "chai";
 
 describe("github issues > #184 [Postgres] Single-Inheritance not working with integer type field", () => {
 
@@ -18,6 +20,7 @@ describe("github issues > #184 [Postgres] Single-Inheritance not working with in
 
     it("single table inheritance should accept a Integer Type", () => Promise.all(connections.map(async connection => {
 
+        // Saving via subtype repository works
         let employeeRepository = connection.getRepository(Employee);
         const employee = new Employee();
         employee.id = "1";
@@ -30,16 +33,28 @@ describe("github issues > #184 [Postgres] Single-Inheritance not working with in
         const loadedEmployee = await employeeRepository.findOneById(1);
         console.log("loaded employee: ", loadedEmployee);
 
+        // let homesitterRepository = connection.getRepository(Homesitter);
+        // const homesitter = new Homesitter();
+        // homesitter.id = "2";
+        // homesitter.firstName = "umed";
+        // homesitter.lastName = "khudoiberdiev";
+        // homesitter.numberOfKids = 5;
+        // homesitter.shared = "h";
+        // await homesitterRepository.persist(homesitter);
 
-        let homesitterRepository = connection.getRepository(Homesitter);
-        const homesitter = new Homesitter();
-        homesitter.id = "2";
-        homesitter.firstName = "umed";
-        homesitter.lastName = "khudoiberdiev";
-        homesitter.numberOfKids = 5;
-        homesitter.shared = "h";
-
-        await homesitterRepository.persist(homesitter);
+        // Saving via base type repository fails
+        try {
+            let personRepository = connection.getRepository(Person);
+            const employee2 = new Employee();
+            employee2.id = "1";
+            employee2.firstName = "umed";
+            employee2.lastName = "khudoiberdiev";
+            employee2.salary = 200000;
+            employee2.shared = "e";
+            personRepository.persist(employee2)
+        } catch (err) {
+            throw err
+        }
 
 
     })));
