f36209117d66e5b055fa37777a51014c74d65f53
Tobias Wolff <Tobias_Wolff@gmx.net>
2017-01-26 11:56:25 +0100

    removing empty method semicolons

--- src/commands/MigrationCreateCommand.ts
+++ src/commands/MigrationCreateCommand.ts
@@ -78,11 +78,9 @@ export class MigrationCreateCommand {
 export class ${name}${timestamp} implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner, connection: Connection, entityManager?: EntityManager): Promise<any> {
-        ;
     }
 
     public async down(queryRunner: QueryRunner, connection: Connection, entityManager?: EntityManager): Promise<any> {
-        ;
     }
 
 }
