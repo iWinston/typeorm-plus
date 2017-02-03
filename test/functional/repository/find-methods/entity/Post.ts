f1ebf6ba850f3c98037ef5794b37b07b5cc91c93
Umed Khudoiberdiev <zarrhost@gmail.com>
2017-02-03 19:13:51 +0500

    returned back acidently removed tests

---
+++ test/functional/repository/find-methods/entity/Post.ts
@@ -0,0 +1,20 @@
+import {Entity} from "../../../../../src/decorator/entity/Entity";
+import {Column} from "../../../../../src/decorator/columns/Column";
+import {PrimaryColumn} from "../../../../../src/decorator/columns/PrimaryColumn";
+
+@Entity()
+export class Post {
+
+    @PrimaryColumn("int")
+    id: number;
+
+    @Column()
+    title: string;
+    
+    @Column()
+    categoryName: string;
+    
+    @Column()
+    isNew: boolean = false;
+
+}
