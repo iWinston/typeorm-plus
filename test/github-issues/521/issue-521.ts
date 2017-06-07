e7ae20370a5f50e93fb1f5f2641d94a237d97554
Bartek <bartek@211-147.eduroam.pwr.wroc.pl>
2017-06-07 19:29:13 +0200

    fix github issue 521 related to typo in QB

---
+++ test/github-issues/521/issue-521.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata";
+import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {Connection} from "../../../src/connection/Connection";
+import {expect} from "chai";
+import {Car} from "./entity/Car";
+
+describe("github issues > #521 Attributes in UPDATE in QB arent getting replaced", () => {
+
+    let connections: Connection[];
+    before(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        schemaCreate: true,
+        dropSchemaOnConnection: true,
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    after(() => closeTestingConnections(connections));
+
+    it("should replace parameters", () => Promise.all(connections.map(async connection => {
+
+        const qb = connection.getRepository(Car).createQueryBuilder("car");
+        const [query, parameters] = qb
+            .update({
+                name: "Honda",
+            })
+            .where("name = :name", {
+                name: "Toyota",
+            })
+            .getSqlWithParameters();
+        return parameters.length.should.eql(2);
+    })));
+
+});
