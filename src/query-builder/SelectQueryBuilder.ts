f99ef2faccc40f759c8f4405f88b36d600786dd1
Zotov Dmitry <dmzt08@gmail.com>
2017-07-05 12:40:34 +0500

    minor fix in locking mechanism;

--- src/query-builder/SelectQueryBuilder.ts
+++ src/query-builder/SelectQueryBuilder.ts
@@ -965,6 +965,7 @@ export class SelectQueryBuilder<Entity> extends QueryBuilder<Entity> {
 
             if (this.expressionMap.lockVersion instanceof Date) {
                 const actualVersion = result[metadata.updateDateColumn!.propertyName]; // what if columns arent set?
+                actualVersion.setMilliseconds(0);
                 this.expressionMap.lockVersion.setMilliseconds(0);
                 if (actualVersion.getTime() !== this.expressionMap.lockVersion.getTime())
                     throw new OptimisticLockVersionMismatchError(metadata.name, this.expressionMap.lockVersion, actualVersion);
