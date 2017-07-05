f5cf34c290a13bf538a5d44bccd11f9ec2303c84
Umed Khudoiberdiev <zarrhost@gmail.com>
2017-07-05 19:24:03 +0500

    added MssqlParameter into main exports

--- src/index.ts
+++ src/index.ts
@@ -75,6 +75,7 @@ export * from "./schema-builder/schema/IndexSchema";
 export * from "./schema-builder/schema/PrimaryKeySchema";
 export * from "./schema-builder/schema/TableSchema";
 export * from "./driver/mongodb/typings";
+export * from "./driver/sqlserver/MssqlParameter";
 
 export {ConnectionOptionsReader} from "./connection/ConnectionOptionsReader";
 export {Connection} from "./connection/Connection";
