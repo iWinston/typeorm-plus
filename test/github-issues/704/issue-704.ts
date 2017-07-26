fb0d1cf53121ba917d3cfe23098cc69f6f225c99
Umed Khudoiberdiev <zarrhost@gmail.com>
2017-07-26 15:01:05 +0500

    test for #704

---
+++ test/github-issues/704/issue-704.ts
@@ -0,0 +1,29 @@
+import "reflect-metadata";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {Connection} from "../../../src/connection/Connection";
+import {User} from "./entity/User";
+
+describe("github issues > #704 Table alias in WHERE clause is not quoted in PostgreSQL", () => {
+
+    let connections: Connection[];
+    before(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        schemaCreate: true,
+        dropSchemaOnConnection: true,
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    after(() => closeTestingConnections(connections));
+
+    it("should return user by a given email and proper escape 'user' keyword", () => Promise.all(connections.map(async connection => {
+
+        const user = new User();
+        user.email = "john@example.com";
+        await connection.manager.save(user);
+
+        const loadedUser = await connection.getRepository(User).findOne({ email: "john@example.com" });
+
+        loadedUser!.id.should.be.equal(1);
+        loadedUser!.email.should.be.equal("john@example.com");
+    })));
+
+});
