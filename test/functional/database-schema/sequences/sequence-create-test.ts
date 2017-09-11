c0a00ad68b52dffeb4da1bd57de8092d01e237a0
Chriss Kalogeropoulos <iz.iznogood@gmail.com>
2017-09-11 12:48:28 +0300

    Fix for issue #855
    We fix the sequence naming in Postgres so that we do not recreate it on each restart.
    Add a test too

---
+++ test/functional/database-schema/sequences/sequence-create-test.ts
@@ -0,0 +1,34 @@
+import "reflect-metadata";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../utils/test-utils";
+import {Connection} from "../../../../src/connection/Connection";
+import {expect} from "chai";
+ 
+import {Person} from "./entity/Person";
+
+describe("sequences > creating a sequence and marking the column as generated", () => {
+
+    let connections: Connection[];
+    before(async () => connections = await createTestingConnections({
+        entities: [Person],
+        enabledDrivers: ["postgres"],
+        schemaCreate: true,
+        dropSchema: true
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    after(() => closeTestingConnections(connections));
+
+    describe("create table and check that primary key column is marked as generated", function() {
+
+        it("should create a non unique index with 2 columns", () => Promise.all(connections.map(async connection => {
+
+            const queryRunner = connection.createQueryRunner();
+            const tableSchema = await queryRunner.loadTableSchema("person");
+            await queryRunner.release();
+
+            expect(tableSchema!.findColumnByName("Id")!.isGenerated).to.be.true;
+
+        })));
+            
+    });
+
+});
