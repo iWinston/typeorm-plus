f64d89741d5a9f8ee088185472005910d3575a0d
Chriss Kalogeropoulos <iz.iznogood@gmail.com>
2017-09-12 16:18:11 +0300

    Add test for issue #863
    Try to create an index and it blows, some problem with propertyPath and referencedColumn

---
+++ test/github-issues/863/issue-863.ts
@@ -0,0 +1,30 @@
+import "reflect-metadata";
+import { closeTestingConnections, createTestingConnections, reloadTestingDatabases } from "../../utils/test-utils";
+import { Connection } from "../../../src/connection/Connection";
+
+import { Master } from "./entities/master";
+import { Detail } from "./entities/detail";
+
+describe("indices > create schema", () => {
+
+    let connections: Connection[];
+    before(async () => connections = await createTestingConnections({
+        entities: [Master, Detail],
+        enabledDrivers: ["mssql"],
+        schemaCreate: false,
+        dropSchema: true
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    after(() => closeTestingConnections(connections));
+
+    describe("build schema", function () {
+
+        it("it should just work, creating the index", () => Promise.all(connections.map(async connection => {
+
+            await connection.synchronize(true);
+
+        })));
+
+    });
+
+});
