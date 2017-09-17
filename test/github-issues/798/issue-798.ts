8297a0d25c1ba8e73fab568df05d165c5074c075
Markus Oskar Lanz <stahlstift@gmail.com>
2017-09-17 18:55:34 +0200

    #798 #799 adding cleanup for unittests

--- test/github-issues/798/issue-798.ts
+++ test/github-issues/798/issue-798.ts
@@ -1,8 +1,10 @@
 import "reflect-metadata";
 import * as assert from "assert";
 import {createConnection, getConnectionOptions} from "../../../src/index";
+import {Connection} from "../../../src/connection/Connection";
 
 describe("github issues > #798 sqlite: 'database' path in ormconfig.json is not relative", () => {
+    let connection: Connection;
     const oldCwd = process.cwd();
 
     before(function () {
@@ -13,9 +15,15 @@ describe("github issues > #798 sqlite: 'database' path in ormconfig.json is not 
         process.chdir(oldCwd);
     });
 
+    afterEach(() => {
+        if (connection && connection.isConnected) {
+            connection.close();
+        }
+    });
+
     it("should find the sqlite database if the cwd is changed", async function () {
         const options = await getConnectionOptions("sqlite");
-        const connection = await createConnection(options);
+        connection = await createConnection(options);
 
         assert.strictEqual(connection.isConnected, true);
     });
--- test/github-issues/799/issue-799.ts
+++ test/github-issues/799/issue-799.ts
@@ -3,8 +3,10 @@ import * as assert from "assert";
 import {createConnection} from "../../../src/index";
 import * as rimraf from "rimraf";
 import {dirname} from "path";
+import {Connection} from "../../../src/connection/Connection";
 
 describe("github issues > #799 sqlite: 'database' path should be created", () => {
+    let connection: Connection;
 
     const path = `${__dirname}/tmp/sqlitedb.db`;
     const cleanup = (done: () => void) => {
@@ -16,8 +18,14 @@ describe("github issues > #799 sqlite: 'database' path should be created", () =>
     before(cleanup);
     after(cleanup);
 
+    afterEach(() => {
+        if (connection && connection.isConnected) {
+            connection.close();
+        }
+    });
+
     it("should create the whole path to database file", async function () {
-        const connection = await createConnection({
+        connection = await createConnection({
             "name": "sqlite",
             "type": "sqlite",
             "database": path
