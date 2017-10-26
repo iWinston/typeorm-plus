544b1b3ba82a1db36e7d0d568fa4c40a5552e9f7
Diluka W <pmars42@gmail.com>
2017-10-26 14:29:26 +0800

    issue #1099 sql generation error with skip condition

---
+++ test/github-issues/1099/issue-1099.ts
@@ -0,0 +1,54 @@
+/**
+ * Created by Diluka on 2017/10/26.
+ *
+ *
+ * ----------- 神 兽 佑 我 -----------
+ *        ┏┓      ┏┓+ +
+ *       ┏┛┻━━━━━━┛┻┓ + +
+ *       ┃          ┃
+ *       ┣     ━    ┃ ++ + + +
+ *      ████━████   ┃+
+ *       ┃          ┃ +
+ *       ┃  ┴       ┃
+ *       ┃          ┃ + +
+ *       ┗━┓      ┏━┛  Code is far away from bug
+ *         ┃      ┃       with the animal protecting
+ *         ┃      ┃ + + + +
+ *         ┃      ┃
+ *         ┃      ┃ +
+ *         ┃      ┃      +  +
+ *         ┃      ┃    +
+ *         ┃      ┗━━━┓ + +
+ *         ┃          ┣┓
+ *         ┃          ┏┛
+ *         ┗┓┓┏━━━━┳┓┏┛ + + + +
+ *          ┃┫┫    ┃┫┫
+ *          ┗┻┛    ┗┻┛+ + + +
+ * ----------- 永 无 BUG ------------
+ */
+import "reflect-metadata";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {Connection} from "../../../src/connection/Connection";
+import {Animal} from "./entity/Animal";
+
+describe("github issues > #1099 BUG - QueryBuilder MySQL skip sql is wrong", () => {
+
+    let connections: Connection[];
+    before(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        enabledDrivers: ["mysql"]
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    after(() => closeTestingConnections(connections));
+
+    it("should skip 1 records", () => Promise.all(connections.map(async connection => {
+        let animals = ["cat", "dog", "bear", "snake"];
+        for (let animal of animals) {
+            await connection.getRepository(Animal).save({name: animal});
+        }
+
+        await connection.getRepository(Animal).createQueryBuilder("a").skip(1).getManyAndCount(); // should not throw error
+
+    })));
+
+});
