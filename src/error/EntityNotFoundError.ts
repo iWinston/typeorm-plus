ff131964c6e6292945be4c07a63aabe709d6fc36
Milano Hadrien <hadrien.milano@gmail.com>
2017-11-28 17:08:02 +0100

    Clean up error management

--- src/error/EntityNotFoundError.ts
+++ src/error/EntityNotFoundError.ts
@@ -1,13 +1,24 @@
+import { ObjectType } from "../";
 /**
  * Thrown when no result could be found in methods which are not allowed to return undefined or an empty set.
  */
 export class EntityNotFoundError extends Error {
     name = "EntityNotFound";
 
-    constructor(entityClass: string, criteria: string) {
+    constructor(entityClass: ObjectType<any>|string, criteria: any) {
         super();
         Object.setPrototypeOf(this, EntityNotFoundError.prototype);
-        this.message = `Could not find any entity of type "${entityClass}" matching: ${criteria}`;
+
+        const className = (typeof entityClass === "string") ? entityClass : entityClass.constructor.name;
+        const criteriaString = this.stringifyCriteria(criteria);
+        this.message = `Could not find any entity of type "${className}" matching: ${criteriaString}`;
+    }
+
+    private stringifyCriteria(criteria: any): string {
+        try {
+            return JSON.stringify(criteria, null, 4);
+        } catch (e) { }
+        return "" + criteria;
     }
 
 }
