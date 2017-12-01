1303fa91fb5f51839f940b669c6e2b43bb6b431b
Umed Khudoiberdiev <zarrhost@gmail.com>
2017-12-01 14:00:13 +0500

    fixed tslint error

--- src/error/FindRelationsNotFoundError.ts
+++ src/error/FindRelationsNotFoundError.ts
@@ -8,7 +8,7 @@ export class FindRelationsNotFoundError extends Error {
         if (notFoundRelations.length === 1) {
             this.message = `Relation "${notFoundRelations[0]}" was not found, please check if it is correct and really exist in your entity.`;
         } else {
-            this.message = `Relations ${notFoundRelations.map(relation => `"${relation}"`).join(", ")} were not found, please check if relations are correct and they exist in your entities.`
+            this.message = `Relations ${notFoundRelations.map(relation => `"${relation}"`).join(", ")} were not found, please check if relations are correct and they exist in your entities.`;
         }
         Object.setPrototypeOf(this, FindRelationsNotFoundError.prototype);
     }
