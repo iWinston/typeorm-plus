38e8cce310038109835216fafee95c383aa061aa
Milano Hadrien <hadrien.milano@gmail.com>
2017-12-08 11:30:56 +0100

    Fix MissingDriverError

--- src/error/MissingDriverError.ts
+++ src/error/MissingDriverError.ts
@@ -1,12 +1,18 @@
+import { ConnectionTypes } from "../connection/ConnectionOptions";
+
 /**
  * Thrown when consumer specifies driver type that does not exist or supported.
  */
 export class MissingDriverError extends Error {
     name = "MissingDriverError";
 
-    constructor(driverType: string) {
+    /**
+     * The parameter type "never" statically ensures that all possible types have been considered before throwing a MissingDriverError.
+     * For instance, the compiler will ensure this can not happen: `Wrong driver: "mysql" given. Supported driver are: ..., "mysql", ...`
+     */
+    constructor(driverType: never) {
         super();
-        this.message = `Wrong driver ${driverType} given. Supported drivers are: "cordova", "mysql", "postgres", "mssql", "oracle", "mariadb", "sqlite".`;
+        this.message = `Wrong driver: "${driverType}" given. Supported drivers are: ${ConnectionTypes.map((t) => "\"" + t + "\"").join(", ")}.`;
         this.stack = new Error().stack;
     }
 
