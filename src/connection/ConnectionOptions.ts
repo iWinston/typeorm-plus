c2a3cd103761b485eb0ed5ea3796b4181774cdc0
Milano Hadrien <hadrien.milano@gmail.com>
2017-12-11 15:35:52 +0100

    Remove super sick type hack

--- src/connection/ConnectionOptions.ts
+++ src/connection/ConnectionOptions.ts
@@ -1,4 +1,3 @@
-import {tuple} from "../util/TypeUtils";
 import {MysqlConnectionOptions} from "../driver/mysql/MysqlConnectionOptions";
 import {PostgresConnectionOptions} from "../driver/postgres/PostgresConnectionOptions";
 import {SqliteConnectionOptions} from "../driver/sqlite/SqliteConnectionOptions";
@@ -24,31 +23,3 @@ export type ConnectionOptions =
     CordovaConnectionOptions|
     SqljsConnectionOptions|
     MongoConnectionOptions;
-
-
-/**
- * List of all available connection types. This list is guaranteed to be exhaustive and correct.
- */
-export const ConnectionTypes = tuple("cordova", "mariadb", "mongodb", "mssql", "mysql", "oracle", "postgres", "sqlite", "sqljs", "websql");
-
-/**
- * This function provides a compile-time verification that the ConnectionTypes tuple above is accurate.
- *
- * If a value is missing in ConnectionTypes or if there is an extraneous value, then this function will
- * not type check.
- *
- * This function should never be called or imported so it can pruned during tree shaking.
- */
-export function __ensureConnectionTypesIsCorrect() {
-    // Force the TS compiler to fetch the full union type across all definitions
-    const a: ConnectionOptions = {} as any;
-    type RealType = typeof a.type;
-
-    // This line ensures all values in the union are acceptable
-    // If there is a compile error here, then there is an extra value in ConnectionTypes or a typo.
-    const b: RealType = ConnectionTypes[0 as number];
-
-    // This line ensures all possible values are listed in ConnectionTypes
-    // If this line gives a compile error, something is missing in ConnectionTypes
-    ConnectionTypes.indexOf(b);
-}
--- src/error/MissingDriverError.ts
+++ src/error/MissingDriverError.ts
@@ -1,18 +1,12 @@
-import { ConnectionTypes } from "../connection/ConnectionOptions";
-
 /**
  * Thrown when consumer specifies driver type that does not exist or supported.
  */
 export class MissingDriverError extends Error {
     name = "MissingDriverError";
 
-    /**
-     * The parameter type "never" statically ensures that all possible types have been considered before throwing a MissingDriverError.
-     * For instance, the compiler will ensure this can not happen: `Wrong driver: "mysql" given. Supported driver are: ..., "mysql", ...`
-     */
-    constructor(driverType: never) {
+    constructor(driverType: string) {
         super();
-        this.message = `Wrong driver: "${driverType}" given. Supported drivers are: ${ConnectionTypes.map((t) => "\"" + t + "\"").join(", ")}.`;
+        this.message = `Wrong driver: "${driverType}" given. Supported drivers are: "cordova", "mariadb", "mongodb", "mssql", "mysql", "oracle", "postgres", "sqlite", "sqljs", "websql".`;
         this.stack = new Error().stack;
     }
 
