8c4933a8e2e6f05703b3a320efc6668df29df89a
Umed Khudoiberdiev <zarrhost@gmail.com>
2017-12-13 12:43:49 +0500

    fixed persistence flag bug in relation metadata, more tests for #1319

--- test/functional/entity-metadata-validator/initialized-relations/validator-intialized-relations.ts
+++ test/functional/entity-metadata-validator/initialized-relations/validator-intialized-relations.ts
@@ -6,10 +6,13 @@ import {expect} from "chai";
 import {InitializedRelationError} from "../../../../src/error/InitializedRelationError";
 import {Category} from "./entity/Category";
 import {Post} from "./entity/Post";
+import {Image} from "./entity/Image";
+import {ImageInfo} from "./entity/ImageInfo";
+import {Question} from "./entity/Question";
 
-describe.only("entity-metadata-validator > initialized relations", () => {
+describe("entity-metadata-validator > initialized relations", () => {
 
-    it("should throw error if relation with initialized array was found", () => {
+    it("should throw error if relation with initialized array was found on many-to-many relation", () => {
         const connection = new Connection({ // dummy connection options, connection won't be established anyway
             type: "mysql",
             host: "localhost",
@@ -22,6 +25,51 @@ describe.only("entity-metadata-validator > initialized relations", () => {
         const entityMetadatas = connectionMetadataBuilder.buildEntityMetadatas([Post, Category], []);
         const entityMetadataValidator = new EntityMetadataValidator();
         expect(() => entityMetadataValidator.validateMany(entityMetadatas, connection.driver)).to.throw(InitializedRelationError);
+    });
+
+    it("should throw error if relation with initialized array was found on one-to-many relation", () => {
+        const connection = new Connection({ // dummy connection options, connection won't be established anyway
+            type: "mysql",
+            host: "localhost",
+            username: "test",
+            password: "test",
+            database: "test",
+            entities: [Image, ImageInfo]
+        });
+        const connectionMetadataBuilder = new ConnectionMetadataBuilder(connection);
+        const entityMetadatas = connectionMetadataBuilder.buildEntityMetadatas([Image, ImageInfo], []);
+        const entityMetadataValidator = new EntityMetadataValidator();
+        expect(() => entityMetadataValidator.validateMany(entityMetadatas, connection.driver)).to.throw(InitializedRelationError);
+    });
+
+    it("should not throw error if relation with initialized array was not found", () => {
+        const connection = new Connection({ // dummy connection options, connection won't be established anyway
+            type: "mysql",
+            host: "localhost",
+            username: "test",
+            password: "test",
+            database: "test",
+            entities: [Category]
+        });
+        const connectionMetadataBuilder = new ConnectionMetadataBuilder(connection);
+        const entityMetadatas = connectionMetadataBuilder.buildEntityMetadatas([Category], []);
+        const entityMetadataValidator = new EntityMetadataValidator();
+        expect(() => entityMetadataValidator.validateMany(entityMetadatas, connection.driver)).not.to.throw(InitializedRelationError);
+    });
+
+    it("should not throw error if relation with initialized array was found, but persistence for this relation was disabled", () => {
+        const connection = new Connection({ // dummy connection options, connection won't be established anyway
+            type: "mysql",
+            host: "localhost",
+            username: "test",
+            password: "test",
+            database: "test",
+            entities: [Question, Category]
+        });
+        const connectionMetadataBuilder = new ConnectionMetadataBuilder(connection);
+        const entityMetadatas = connectionMetadataBuilder.buildEntityMetadatas([Question, Category], []);
+        const entityMetadataValidator = new EntityMetadataValidator();
+        expect(() => entityMetadataValidator.validateMany(entityMetadatas, connection.driver)).not.to.throw(InitializedRelationError);
     });
 
 });
