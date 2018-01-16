c05a1e89674d9bade306a0c085615d21ab8ebb09
Umed Khudoiberdiev <zarrhost@gmail.com>
2018-01-16 14:28:33 +0500

    fixed failing tests

--- test/functional/entity-metadata-validator/entity-metadata-validator.ts
+++ test/functional/entity-metadata-validator/entity-metadata-validator.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import {Connection} from "../../../src/connection/Connection";
 import {ConnectionMetadataBuilder} from "../../../src/connection/ConnectionMetadataBuilder";
 import {EntityMetadataValidator} from "../../../src/metadata-builder/EntityMetadataValidator";
+import {expect} from "chai";
 
 describe("entity-metadata-validator", () => {
 
