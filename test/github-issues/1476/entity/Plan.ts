6cee3cc32753382ec3440bac7e36991e2492458b
Daniel Lang <ldaniell14260@gmail.com>
2018-01-23 21:43:06 +0100

    fix(query-builder): leftJoinAndSelect with subquery fixed

---
+++ test/github-issues/1476/entity/Plan.ts
@@ -0,0 +1,11 @@
+import { Entity, PrimaryColumn, Column } from "../../../../src/index";
+
+
+@Entity()
+export class Plan {
+    @PrimaryColumn()
+    planId: number;
+
+    @Column()
+    planName: string;
+}
