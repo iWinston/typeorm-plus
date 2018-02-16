40c68483c506778e8f4c1d7dfc7ba74e4f6438d4
Yeonghoon Park <me@yhpark.io>
2018-02-17 00:54:27 +0900

    Handle SQLite in-memory db in ConnectionOptionsReader

--- test/functional/connection-options-reader/connection-options-reader.ts
+++ test/functional/connection-options-reader/connection-options-reader.ts
@@ -2,7 +2,7 @@ import {expect} from "chai";
 import {ConnectionOptions} from "../../../src/connection/ConnectionOptions";
 import {ConnectionOptionsReader} from "../../../src/connection/ConnectionOptionsReader";
 
-describe("ConnectionOptionsReader", () => {
+describe.only("ConnectionOptionsReader", () => {
   it("properly loads config with entities specified", async () => {
     type EntititesList = Function[] | string[];
     const connectionOptionsReader = new ConnectionOptionsReader({ root: __dirname, configName: "configs/class-entities" });
@@ -11,4 +11,14 @@ describe("ConnectionOptionsReader", () => {
     const entities: EntititesList = options.entities as EntititesList;
     expect(entities.length).to.equal(1);
   });
+
+  it("properly loads sqlite in-memory/path config", async () => {
+    const connectionOptionsReader = new ConnectionOptionsReader({ root: __dirname, configName: "configs/sqlite-memory" });
+    const inmemoryOptions: ConnectionOptions = await connectionOptionsReader.get("memory");
+    expect(inmemoryOptions.database).to.equal(":memory:");
+    const fileOptions: ConnectionOptions = await connectionOptionsReader.get("file");
+    expect(fileOptions.database).to.have.string("/test");
+  });
+
+
 });
