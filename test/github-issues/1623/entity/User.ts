b355a2ff4207edd336676aa7f40915bddad81f94
Zotov Dmitry <dmzt08@gmail.com>
2018-02-22 18:19:44 +0500

    added test (issue #1623)

---
+++ test/github-issues/1623/entity/User.ts
@@ -0,0 +1,18 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "../../../../src";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    age: number;
+
+}
