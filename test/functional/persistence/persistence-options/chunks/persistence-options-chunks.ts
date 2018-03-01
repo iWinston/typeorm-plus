8004da29c3dccba92ed5d6e9893e0837d8ef66ee
Zotov Dmitry <dmzt08@gmail.com>
2018-03-01 19:22:31 +0500

    fixed falling test;

--- test/functional/persistence/persistence-options/chunks/persistence-options-chunks.ts
+++ test/functional/persistence/persistence-options/chunks/persistence-options-chunks.ts
@@ -2,9 +2,8 @@ import "reflect-metadata";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../../utils/test-utils";
 import {Post} from "./entity/Post";
 import {Connection} from "../../../../../src/connection/Connection";
-// import {expect} from "chai";
 
-describe.only("persistence > persistence options > chunks", () => {
+describe("persistence > persistence options > chunks", () => {
 
     // -------------------------------------------------------------------------
     // Configuration
@@ -21,13 +20,13 @@ describe.only("persistence > persistence options > chunks", () => {
 
     it("should save objects in chunks", () => Promise.all(connections.map(async connection => {
         const posts: Post[] = [];
-        for (let i = 0; i < 25000; i++) {
+        for (let i = 0; i < 25000; i++) { // CI falls on Node 4 with 100000 rows
             const post = new Post();
             post.title = "Bakhrom " + i;
             post.description = "Hello" + i;
             posts.push(post);
         }
-        await connection.manager.save(posts, { chunk: 5000 });
+        await connection.manager.save(posts, { chunk: 5000 }); // CI falls on Node 4 with 10000 chunks
     })));
 
 });
