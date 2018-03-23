8b000138d80f4ee8fbe2dd3fa388dbc19449250e
Zotov Dmitry <dmzt08@gmail.com>
2018-03-23 11:57:08 +0500

    debugging CI falling test;

--- test/github-issues/1736/issue-1736.ts
+++ test/github-issues/1736/issue-1736.ts
@@ -1,16 +1,20 @@
 import "reflect-metadata";
 import {DateUtils} from "../../../src/util/DateUtils";
+import {expect} from "chai";
 
-describe("github issues > #1736 Milliseconds are parsed incorrectly from datetime string", () => {
+describe.only("github issues > #1736 Milliseconds are parsed incorrectly from datetime string", () => {
 
     it("should correctly parse milliseconds from datetime string", () =>  {
-        DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.002Z").should.be.equal("2018-03-10 21:13:08.002");
-        DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.020Z").should.be.equal("2018-03-10 21:13:08.020");
-        DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.200Z").should.be.equal("2018-03-10 21:13:08.200");
+        expect(DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.002Z")).to.be.equal("2018-03-10 21:13:08.002");
+        expect(DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.020Z")).to.be.equal("2018-03-10 21:13:08.020");
+        expect(DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.200Z")).to.be.equal("2018-03-10 21:13:08.200");
 
-        DateUtils.mixedDateToUtcDatetimeString("2018-03-10T16:13:08.002Z").should.be.equal("2018-03-10 16:13:08.002");
-        DateUtils.mixedDateToUtcDatetimeString("2018-03-10T16:13:08.020Z").should.be.equal("2018-03-10 16:13:08.020");
-        DateUtils.mixedDateToUtcDatetimeString("2018-03-10T16:13:08.200Z").should.be.equal("2018-03-10 16:13:08.200");
+        // expect(DateUtils.mixedDateToUtcDatetimeString("2018-03-10T16:13:08.002Z")).to.be.equal("2018-03-10 16:13:08.002");
+        // console.log(DateUtils.mixedDateToUtcDatetimeString("2018-03-10T16:13:08.002Z"));
+        // expect(DateUtils.mixedDateToUtcDatetimeString("2018-03-10T16:13:08.020Z")).to.be.equal("2018-03-10 16:13:08.020");
+        // console.log(DateUtils.mixedDateToUtcDatetimeString("2018-03-10T16:13:08.020Z"));
+        // expect(DateUtils.mixedDateToUtcDatetimeString("2018-03-10T16:13:08.200Z")).to.be.equal("2018-03-10 16:13:08.200");
+        // console.log(DateUtils.mixedDateToUtcDatetimeString("2018-03-10T16:13:08.200Z"));
     });
 
 });
