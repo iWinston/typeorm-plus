fe1cb8ef952f4ac338c173d8ec2e1b62cdc5bd29
Zotov Dmitry <dmzt08@gmail.com>
2018-03-23 12:04:23 +0500

    debugging CI falling test;

--- test/github-issues/1736/issue-1736.ts
+++ test/github-issues/1736/issue-1736.ts
@@ -6,8 +6,13 @@ describe.only("github issues > #1736 Milliseconds are parsed incorrectly from da
 
     it("should correctly parse milliseconds from datetime string", () =>  {
         expect(DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.002Z")).to.be.equal("2018-03-10 21:13:08.002");
+        console.log(DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.002Z"));
+
         expect(DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.020Z")).to.be.equal("2018-03-10 21:13:08.020");
+        console.log(DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.020Z"));
+
         expect(DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.200Z")).to.be.equal("2018-03-10 21:13:08.200");
+        console.log(DateUtils.mixedDateToDatetimeString("2018-03-10T16:13:08.200Z"));
 
         // expect(DateUtils.mixedDateToUtcDatetimeString("2018-03-10T16:13:08.002Z")).to.be.equal("2018-03-10 16:13:08.002");
         // console.log(DateUtils.mixedDateToUtcDatetimeString("2018-03-10T16:13:08.002Z"));
