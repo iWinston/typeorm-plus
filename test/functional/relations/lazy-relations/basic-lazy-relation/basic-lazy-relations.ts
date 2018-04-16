a63a5a6dd62e881c7924b31d3bd01acc0120f5d7
Aiden McClelland <amcclelland@saltlending.com>
2018-04-16 13:56:44 -0600

    testing on cli

--- test/functional/relations/lazy-relations/basic-lazy-relation/basic-lazy-relations.ts
+++ test/functional/relations/lazy-relations/basic-lazy-relation/basic-lazy-relations.ts
@@ -339,7 +339,7 @@ describe("basic-lazy-relations", () => {
             });
             await manager.save(category);
             const loadedCategory = await connection.manager.findOne(Category, { where: { name: "test category" } });
-            console.error("here");
+            throw new Error("here");
             const loadedPost = await loadedCategory!.onePost;
             loadedPost.title.should.be.equal("test post");
         });
