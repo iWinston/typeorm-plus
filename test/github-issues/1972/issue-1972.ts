e605b34594b65a92951691a1fc711adff5ef42a3
Jan Grądzki <gradzki.jan@gmail.com>
2018-04-23 13:44:18 +0200

    added test for #1972

---
+++ test/github-issues/1972/issue-1972.ts
@@ -0,0 +1,64 @@
+import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {Connection} from "../../../src/connection/Connection";
+import {assert} from "chai";
+
+import { User } from "./entity/User";
+import { TournamentUserParticipant } from "./entity/TournamentUserParticipant";
+import { TournamentSquadParticipant } from "./entity/TournamentSquadParticipant";
+
+describe("github issues > #1972 STI problem - empty columns", () => {
+    let connections: Connection[];
+
+    before(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        schemaCreate: true,
+        dropSchema: true,
+    }));
+    
+    beforeEach(() => reloadTestingDatabases(connections));
+
+    after(() => closeTestingConnections(connections));
+
+    it("should insert with userId", () => Promise.all(connections.map(async connection => {
+        // create user
+        const user = new User({
+            name: "test",
+        });
+        await connection.manager.save(user);
+
+        // create user participant
+        const tournamentUserParticipant = new TournamentUserParticipant({
+            user,
+        });
+        await connection.manager.save(tournamentUserParticipant);
+
+        // find user participant in the DB
+        const result = await connection.manager.findOne(TournamentUserParticipant);
+
+        if (result) {
+            assert(result.user instanceof User);
+        }
+    })));
+
+    it("should insert with ownerId", () => Promise.all(connections.map(async connection => {
+        // create user
+        const user = new User({
+            name: "test",
+        });
+        await connection.manager.save(user);
+
+        // create tournament squad participant
+        const tournamentSquadParticipant = new TournamentSquadParticipant({
+            users: [ user ],
+            owner: user,
+        });
+        await connection.manager.save(tournamentSquadParticipant);
+
+        // find squad participant in the DB
+        const result = await connection.manager.findOne(TournamentSquadParticipant);
+
+        if (result) {
+            assert(result.owner instanceof User);
+        }
+    })));
+});
