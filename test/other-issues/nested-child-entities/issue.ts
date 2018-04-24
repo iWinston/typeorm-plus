f3ce25acfab494303ad899f04b03c85d711afab3
Jan Grądzki <gradzki.jan@gmail.com>
2018-04-24 15:44:13 +0200

    added test for nested child entities

---
+++ test/other-issues/nested-child-entities/issue.ts
@@ -0,0 +1,30 @@
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {Connection} from "../../../src";
+
+import {TournamentGraph} from "./entity/TournamentGraph";
+import {SquadBilliardsTournament} from "./entity/SquadBilliardsTournament";
+
+describe("other issues > using nested child entities", () => {
+    let connections: Connection[];
+
+    before(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+    }));
+    
+    beforeEach(() => reloadTestingDatabases(connections));
+
+    after(() => closeTestingConnections(connections));
+
+    it("should insert without error", () => Promise.all(connections.map(async connection => {
+        const squadBilliardsTournament = new SquadBilliardsTournament({
+            name: "Squad Tournament",
+        });
+
+        await connection.manager.save(squadBilliardsTournament);
+        const tournamentGraph = new TournamentGraph();
+
+        tournamentGraph.tournament = squadBilliardsTournament;
+
+        await connection.manager.save(tournamentGraph);
+    })));
+});
