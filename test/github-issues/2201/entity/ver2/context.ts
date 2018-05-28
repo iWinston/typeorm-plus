74b6fe57c66f2330612e8a7bd9c9eb7e22b9a9d5
nitzantomer <nitzan.tomer@gmail.com>
2018-05-28 16:33:08 +0300

    two issues:  1. when naming the primary columns in a junction table it creates two columns per one: 'userId' and 'user_id'  2. when updating the main entity ('Record'), the ORM tries to update the junction table as well, twice, and the 2nd time it fails.     there's no reason to do so even once.

---
+++ test/github-issues/2201/entity/ver2/context.ts
@@ -0,0 +1,24 @@
+import { Column, PrimaryColumn, ManyToOne } from "../../../../../src/index";
+import { Entity } from "../../../../../src/decorator/entity/Entity";
+import { BaseEntity } from "../../../../../src/repository/BaseEntity";
+
+import { User } from "./user";
+import { Record } from "./record";
+
+@Entity({ name: "record_contexts" })
+export class RecordContext extends BaseEntity {
+    @PrimaryColumn()
+    recordId: string;
+
+    @PrimaryColumn()
+    userId: string;
+
+    @ManyToOne(type => Record, record => record.contexts)
+    public readonly record: Record;
+
+    @ManyToOne(type => User, user => user.contexts)
+    public readonly user: User;
+
+    @Column("simple-json")
+    public readonly meta: any;
+}
