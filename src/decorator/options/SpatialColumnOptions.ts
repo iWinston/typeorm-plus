a266a5a1ce6d80a72d120ae4823fe95e7f48ac0e
Seth Fitzsimmons <seth@mojodna.net>
2018-06-29 11:43:02 -0700

    Add missing interface

---
+++ src/decorator/options/SpatialColumnOptions.ts
@@ -0,0 +1,18 @@
+/**
+ * Options for spatial columns.
+ */
+export interface SpatialColumnOptions {
+
+    /**
+     * Column type's feature type.
+     * Geometry, Point, Polygon, etc.
+     */
+    spatialFeatureType?: string;
+
+    /**
+     * Column type's SRID.
+     * Spatial Reference ID or EPSG code.
+     */
+    srid?: number;
+
+}
