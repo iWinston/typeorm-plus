5df2f24279641b992e99266e917720ba2771b05a
Chris Kalmar <christian.kalmar@gmail.com>
2018-08-08 19:31:32 +0200

    define test entity

---
+++ test/github-issues/2128/entity/Post.ts
@@ -0,0 +1,17 @@
+import { PrimaryGeneratedColumn, Entity, Column } from "../../../../src";
+
+@Entity()
+export class Post {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column({
+        type: "json"
+    })
+    meta: any;
+
+}
