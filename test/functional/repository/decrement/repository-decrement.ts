ddefed6315924a084e85e17d047216b0833f0863
Vladimir Poluch <vlado@zwizu.com>
2018-12-06 11:34:02 +0100

    Fix #2288 - Increment/decrement accept string as input value

--- test/functional/repository/decrement/repository-decrement.ts
+++ test/functional/repository/decrement/repository-decrement.ts
@@ -1,69 +1,169 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../utils/test-utils";
-import {Connection} from "../../../../src/connection/Connection";
-import {Post} from "./entity/Post";
+import { closeTestingConnections, createTestingConnections, reloadTestingDatabases } from "../../../utils/test-utils";
+import { Connection } from "../../../../src/connection/Connection";
+import { Post } from "./entity/Post";
+import { PostBigInt } from "./entity/PostBigInt";
 
 describe("repository > decrement method", () => {
 
-    let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
-        entities: [Post],
-    }));
-    beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    describe("basic", () => {
 
-    it("should decrement value", () => Promise.all(connections.map(async connection => {
+        let connections: Connection[];
+        before(async () => connections = await createTestingConnections({
+            entities: [Post]
+        }));
+        beforeEach(() => reloadTestingDatabases(connections));
+        after(() => closeTestingConnections(connections));
 
-        // save few dummy posts
-        const post1 = new Post();
-        post1.id = 1;
-        post1.title = "post #1";
-        post1.counter = 2;
+        it("should decrement value", () => Promise.all(connections.map(async connection => {
 
-        const post2 = new Post();
-        post2.id = 2;
-        post2.title = "post #2";
-        post2.counter = 5;
-        await connection.manager.save([post1, post2]);
+            // save few dummy posts
+            const post1 = new Post();
+            post1.id = 1;
+            post1.title = "post #1";
+            post1.counter = 2;
+            const post2 = new Post();
+            post2.id = 2;
+            post2.title = "post #2";
+            post2.counter = 5;
+            await connection.manager.save([post1, post2]);
 
-        // decrement counter of post 1
-        await connection
-            .getRepository(Post)
-            .decrement({ id: 1 }, "counter", 1);
+            // decrement counter of post 1
+            await connection
+                .getRepository(Post)
+                .decrement({ id: 1 }, "counter", 1);
 
-        // decrement counter of post 2
-        await connection
-            .manager
-            .decrement(Post, { id: 2 }, "counter", 2);
+            // decrement counter of post 2
+            await connection
+                .manager
+                .decrement(Post, { id: 2 }, "counter", 3);
 
-        // load and check counter
-        const loadedPost1 = await connection.manager.findOne(Post, 1);
-        loadedPost1!.counter.should.be.equal(1);
+            // load and check counter
+            const loadedPost1 = await connection.manager.findOne(Post, 1);
+            loadedPost1!.counter.should.be.equal(1);
 
-        const loadedPost2 = await connection.manager.findOne(Post, 2);
-        loadedPost2!.counter.should.be.equal(3);
-    })));
+            const loadedPost2 = await connection.manager.findOne(Post, 2);
+            loadedPost2!.counter.should.be.equal(2);
+        })));
 
-    it("should throw an error if column property path was not found", () => Promise.all(connections.map(async connection => {
+        it("should accept string as input and decrement value", () => Promise.all(connections.map(async connection => {
 
-        // save few dummy posts
-        const post1 = new Post();
-        post1.id = 1;
-        post1.title = "post #1";
-        post1.counter = 1;
+            // save few dummy posts
+            const post1 = new Post();
+            post1.id = 1;
+            post1.title = "post #1";
+            post1.counter = 23;
+            const post2 = new Post();
+            post2.id = 2;
+            post2.title = "post #2";
+            post2.counter = 35;
+            await connection.manager.save([post1, post2]);
 
-        const post2 = new Post();
-        post2.id = 2;
-        post2.title = "post #2";
-        post2.counter = 1;
-        await connection.manager.save([post1, post2]);
+            // decrement counter of post 1
+            await connection
+                .getRepository(Post)
+                .decrement({ id: 1 }, "counter", "22");
 
-        // decrement counter of post 1
-        await connection
-            .getRepository(Post)
-            .decrement({ id: 1 }, "counters", 1)
-            .should.be.rejected;
+            // decrement counter of post 2
+            await connection
+                .manager
+                .decrement(Post, { id: 2 }, "counter", "33");
 
-    })));
+            // load and check counter
+            const loadedPost1 = await connection.manager.findOne(Post, 1);
+            loadedPost1!.counter.should.be.equal(1);
+
+            const loadedPost2 = await connection.manager.findOne(Post, 2);
+            loadedPost2!.counter.should.be.equal(2);
+        })));
+
+        it("should throw an error if column property path was not found", () => Promise.all(connections.map(async connection => {
+
+            // save few dummy posts
+            const post1 = new Post();
+            post1.id = 1;
+            post1.title = "post #1";
+            post1.counter = 1;
+            const post2 = new Post();
+            post2.id = 2;
+            post2.title = "post #2";
+            post2.counter = 1;
+            await connection.manager.save([post1, post2]);
+
+            // decrement counter of post 1
+            await connection
+                .getRepository(Post)
+                .decrement({ id: 1 }, "unknownProperty", 1)
+                .should.be.rejected;
+
+        })));
+
+        it("should throw an error if input value is not number", () => Promise.all(connections.map(async connection => {
+
+            // save few dummy posts
+            const post1 = new Post();
+            post1.id = 1;
+            post1.title = "post #1";
+            post1.counter = 1;
+            const post2 = new Post();
+            post2.id = 2;
+            post2.title = "post #2";
+            post2.counter = 1;
+            await connection.manager.save([post1, post2]);
+
+            // decrement counter of post 1
+            await connection
+                .getRepository(Post)
+                .decrement({ id: 1 }, "counter", "12abc")
+                .should.be.rejected;
+
+        })));
+
+    });
+
+    describe("bigint", () => {
+
+        let connections: Connection[];
+        before(async () => connections = await createTestingConnections({
+            entities: [PostBigInt],
+            enabledDrivers: ["mysql", "mariadb", "postgres"],
+            // logging: true
+        }));
+        beforeEach(() => reloadTestingDatabases(connections));
+        after(() => closeTestingConnections(connections));
+
+        it("should decrement value", () => Promise.all(connections.map(async connection => {
+
+            // save few dummy posts
+            const postBigInt1 = new PostBigInt();
+            postBigInt1.id = 1;
+            postBigInt1.title = "post #1";
+            postBigInt1.counter = "9000000000000000001";
+            const postBigInt2 = new PostBigInt();
+            postBigInt2.id = 2;
+            postBigInt2.title = "post #2";
+            postBigInt2.counter = "9000000000000000002";
+            await connection.manager.save([postBigInt1, postBigInt2]);
+
+            // decrement counter of post 1
+            await connection
+                .getRepository(PostBigInt)
+                .decrement({ id: 1 }, "counter", "9000000000000000000");
+
+            // decrement counter of post 2
+            await connection
+                .manager
+                .decrement(PostBigInt, { id: 2 }, "counter", "9000000000000000000");
+
+            // load and check counter
+            const loadedPost1 = await connection.manager.findOne(PostBigInt, 1);
+            loadedPost1!.counter.should.be.equal("1");
+
+            const loadedPost2 = await connection.manager.findOne(PostBigInt, 2);
+            loadedPost2!.counter.should.be.equal("2");
+
+        })));
+
+    });
 
 });
