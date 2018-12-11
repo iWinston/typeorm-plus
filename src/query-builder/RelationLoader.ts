201f291784894eaf285b155b374992bf6f2a8e97
Joel Gallant <joel@joelgallant.me>
2018-12-11 16:37:18 -0700

    Uses defineProperty so that __{property}__, __has_ and __promise_ variables are not enumerable

--- src/query-builder/RelationLoader.ts
+++ src/query-builder/RelationLoader.ts
@@ -189,6 +189,7 @@ export class RelationLoader {
         const promiseIndex = "__promise_" + relation.propertyName + "__"; // in what property of the entity loading promise will be stored
         const resolveIndex = "__has_" + relation.propertyName + "__"; // indicates if relation data already was loaded or not, we need this flag if loaded data is empty
 
+        // using defineProperty in these so that private properties are enumerable: false
         Object.defineProperty(entity, relation.propertyName, {
             get: function() {
                 if (this[resolveIndex] === true || this[dataIndex]) // if related data already was loaded then simply return it
@@ -198,28 +199,56 @@ export class RelationLoader {
                     return this[promiseIndex];
 
                 // nothing is loaded yet, load relation data and save it in the model once they are loaded
-                this[promiseIndex] = relationLoader.load(relation, this, queryRunner).then(result => {
-                    if (relation.isOneToOne || relation.isManyToOne) result = result[0];
-                    this[dataIndex] = result;
-                    this[resolveIndex] = true;
-                    delete this[promiseIndex];
-                    return this[dataIndex];
+                Object.defineProperty(this, promiseIndex, {
+                    configurable: true,
+                    value: relationLoader.load(relation, this, queryRunner).then(result => {
+                        if (relation.isOneToOne || relation.isManyToOne) result = result[0];
+
+                        Object.defineProperty(this, dataIndex, {
+                            value: result,
+                            writable: true,
+                        });
+
+                        Object.defineProperty(this, resolveIndex, {
+                            value: true,
+                            writable: true,
+                        });
+
+                        delete this[promiseIndex];
+                        return this[dataIndex];
+                    }),
                 });
+
                 return this[promiseIndex];
             },
             set: function(value: any|Promise<any>) {
-                if (value instanceof Promise) { // if set data is a promise then wait for its resolve and save in the object
+                if (value instanceof Promise) {
+                    // if set data is a promise then wait for its resolve and save in the object
                     value.then(result => {
-                        this[dataIndex] = result;
-                        this[resolveIndex] = true;
+                        Object.defineProperty(this, dataIndex, {
+                            value: result,
+                            writable: true,
+                        });
+
+                        Object.defineProperty(this, resolveIndex, {
+                            value: true,
+                            writable: true,
+                        });
+                    });
+                } else {
+                    // if its direct data set (non promise, probably not safe-typed)
+                    Object.defineProperty(this, dataIndex, {
+                        value,
+                        writable: true,
                     });
 
-                } else { // if its direct data set (non promise, probably not safe-typed)
-                    this[dataIndex] = value;
-                    this[resolveIndex] = true;
+                    Object.defineProperty(this, resolveIndex, {
+                        value: true,
+                        writable: true,
+                    });
                 }
             },
-            configurable: true
+            configurable: true,
         });
     }
 
