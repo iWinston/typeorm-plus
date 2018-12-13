b2faf0826d60a7f18077988a5f12f0e90ae520a8
Umed Khudoiberdiev <pleerock.me@gmail.com>
2018-12-13 12:43:36 +0500

    Revert "Fix uuid length error for issue #1139"

--- test/github-issues/1139/entity/User.ts
+++
@@ -1,8 +0,0 @@
-import {Entity} from "../../../../src/decorator/entity/Entity";
-import {PrimaryGeneratedColumn} from "../../../../src/decorator/columns/PrimaryGeneratedColumn";
-
-@Entity()
-export class User {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
-}
