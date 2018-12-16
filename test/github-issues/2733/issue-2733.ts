99840791d1fd30ef91b036ad76a63be5deaf1af4
Kononnable <kononnable@gmail.com>
2018-12-16 03:00:18 +0100

    fixes for postgres and tests for other db engines which support functions as defuaults

--- test/github-issues/2733/issue-2733.ts
+++ test/github-issues/2733/issue-2733.ts
@@ -3,25 +3,40 @@ import { createTestingConnections, closeTestingConnections, reloadTestingDatabas
 import { Connection } from "../../../src/connection/Connection";
 import { expect } from "chai";
 
-describe("github issues > #2733 SqlServer should correctly handle function calls as default values", () => {
+describe("github issues > #2733 should correctly handle function calls with upercase letters as default values", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
-        entities: [__dirname + "/entity/*{.js,.ts}"],
-        schemaCreate: true,
-        dropSchema: true,
-        enabledDrivers: ["mssql"],
-    }));
-    beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
 
-    it("should not change already synchronized entities", () => Promise.all(connections.map(async connection => {
-
-        const schemaBuilder = connection.driver.createSchemaBuilder();
-        const syncQueries = await schemaBuilder.log();
-
-        expect(syncQueries.downQueries).to.be.empty;
-        expect(syncQueries.upQueries).to.be.empty;
-    })));
-
+    it("MSSQL, Sqljs, Sqlite", async () => {
+        connections = await createTestingConnections({
+            entities: [__dirname + "/entity/MSSQLDummy{.js,.ts}"],
+            schemaCreate: true,
+            dropSchema: true,
+            enabledDrivers: ["mssql", "sqljs", "sqlite"],
+        });
+        await reloadTestingDatabases(connections);
+        await Promise.all(connections.map(async connection => {
+            const schemaBuilder = connection.driver.createSchemaBuilder();
+            const syncQueries = await schemaBuilder.log();
+            expect(syncQueries.downQueries).to.be.empty;
+            expect(syncQueries.upQueries).to.be.empty;
+        }));
+        await closeTestingConnections(connections);
+    });
+    it("Postgres", async () => {
+        connections = await createTestingConnections({
+            entities: [__dirname + "/entity/PostgresDummy{.js,.ts}"],
+            schemaCreate: true,
+            dropSchema: true,
+            enabledDrivers: ["postgres"],
+        });
+        await reloadTestingDatabases(connections);
+        await Promise.all(connections.map(async connection => {
+            const schemaBuilder = connection.driver.createSchemaBuilder();
+            const syncQueries = await schemaBuilder.log();
+            expect(syncQueries.downQueries).to.be.empty;
+            expect(syncQueries.upQueries).to.be.empty;
+        }));
+        await closeTestingConnections(connections);
+    });
 });
