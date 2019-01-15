27fcea3ce52b4f8814b84573834a90a1dac8aa66
Umed Khudoiberdiev <pleerock.me@gmail.com>
2019-01-15 10:45:34 +0500

    fixing tslint

--- test/github-issues/3422/entity/User.ts
+++ test/github-issues/3422/entity/User.ts
@@ -1,8 +1,8 @@
 import {Entity} from "../../../../src/decorator/entity/Entity";
 import {PrimaryGeneratedColumn} from "../../../../src/decorator/columns/PrimaryGeneratedColumn";
-import {Tree} from "../../../../src/decorator/tree/Tree"
-import {TreeChildren} from "../../../../src/decorator/tree/TreeChildren"
-import {TreeParent} from "../../../../src/decorator/tree/TreeParent"
+import {Tree} from "../../../../src/decorator/tree/Tree";
+import {TreeChildren} from "../../../../src/decorator/tree/TreeChildren";
+import {TreeParent} from "../../../../src/decorator/tree/TreeParent";
 
 @Entity({ name: "users", schema: "admin" })
 @Tree("nested-set")
