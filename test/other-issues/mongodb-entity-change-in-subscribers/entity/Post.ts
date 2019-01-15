aa1d7385d50ce10757a6e646787176d71b2138e6
Rustam Mamadaminov <rmamdaminov@gmail.com>
2019-01-15 14:57:02 +0500

    Tests for mongodb

---
+++ test/other-issues/mongodb-entity-change-in-subscribers/entity/Post.ts
@@ -0,0 +1,24 @@
+import {Entity} from "../../../../src/decorator/entity/Entity";
+import {Column} from "../../../../src/decorator/columns/Column";
+import {UpdateDateColumn} from "../../../../src/decorator/columns/UpdateDateColumn";
+import {ObjectIdColumn} from "../../../../src";
+
+@Entity()
+export class Post {
+
+    @ObjectIdColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column({default: false})
+    active: boolean;
+
+    @UpdateDateColumn()
+    updateDate: Date;
+
+    updatedColumns: number = 0;
+
+    loaded: boolean = false;
+}
