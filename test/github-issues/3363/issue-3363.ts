5271bd59b1060085daabf40a3662eae6f828b1e5
Victor Schinaider <victorschinaider@outlook.com>
2019-01-16 20:51:19 -0300

    fix

--- test/github-issues/3363/issue-3363.ts
+++ test/github-issues/3363/issue-3363.ts
@@ -5,7 +5,7 @@ import {Post} from "./entity/Post";
 import {expect} from "chai";
 import { Category } from "./entity/Category";
 
-describe.only("github issues > #3363 Isolation Level in transaction() from Connection", () => {
+describe("github issues > #3363 Isolation Level in transaction() from Connection", () => {
 
     let connections: Connection[];
     before(async () => connections = await createTestingConnections({
