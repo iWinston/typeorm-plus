00a9f7e52c24f15192e53952c7f569c1416ed311
Victor Schinaider <victorschinaider@outlook.com>
2019-01-15 21:45:36 -0300

    tests for issue 3363

---
+++ test/github-issues/3363/entity/Post.ts
@@ -0,0 +1,16 @@
+import {Column} from "../../../../src/decorator/columns/Column";
+import {Entity} from "../../../../src/decorator/entity/Entity";
+import { PrimaryGeneratedColumn } from "../../../../src/decorator/columns/PrimaryGeneratedColumn";
+
+@Entity()
+export class Post {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        type: "text",
+    })
+    title: string;
+
+}
