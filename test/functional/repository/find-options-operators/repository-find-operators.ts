69acbfbc6df2142ffb7617fd7d2341e45f90e59f
Zotov Dmitry <dmzt08@gmail.com>
2019-02-23 16:46:23 +0500

    testing falling test for crdb only;

--- test/functional/repository/find-options-operators/repository-find-operators.ts
+++ test/functional/repository/find-options-operators/repository-find-operators.ts
@@ -12,8 +12,7 @@ describe("repository > find options > operators", () => {
     let connections: Connection[];
     before(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
-        schemaCreate: true,
-        dropSchema: true,
+        enabledDrivers: ["cockroachdb"]
     }));
     beforeEach(() => reloadTestingDatabases(connections));
     after(() => closeTestingConnections(connections));
