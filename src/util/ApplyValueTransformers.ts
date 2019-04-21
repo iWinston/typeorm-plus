6728c5d1e32919b2ccb64bfc8370d1e552a3aad1
Sammy Teillet <sammy@MacBook-Pro-de-Sammy.local>
2019-04-21 19:23:18 +0200

    feat: Support multiple transformers

--- src/util/ApplyValueTransformers.ts
+++ src/util/ApplyValueTransformers.ts
@@ -3,13 +3,17 @@ import { ValueTransformer } from "../decorator/options/ValueTransformer";
 export class ApplyValueTransformers {
     static innerTransform(transformer: ValueTransformer | ValueTransformer[], databaseValue: any) {
         if (Array.isArray(transformer)) {
-            return transformer[0].from(transformer[1].from(databaseValue));
+            return transformer.reverse().reduce((transformedValue, _transformer) => {
+                return _transformer.from(transformedValue);
+            }, databaseValue);
         }
         return transformer.from(databaseValue);
     }
     static outerTransform(transformer: ValueTransformer | ValueTransformer[], entityValue: any) {
         if (Array.isArray(transformer)) {
-            return transformer[1].to(transformer[0].to(entityValue));
+            return transformer.reduce((transformedValue, _transformer) => {
+                return _transformer.to(transformedValue);
+            }, entityValue);
         }
         return transformer.to(entityValue);
     }
