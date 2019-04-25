bdadf0b6dbce63ae08b6f6f4522a9f37894762d6
Rustam Mamadaminov <rmamdaminov@gmail.com>
2019-04-25 11:01:11 +0500

    fix

--- src/schema-builder/MongoSchemaBuilder.ts
+++ src/schema-builder/MongoSchemaBuilder.ts
@@ -39,7 +39,14 @@ export class MongoSchemaBuilder implements SchemaBuilder {
         const promises: Promise<any>[] = [];
         this.connection.entityMetadatas.forEach(metadata => {
             metadata.indices.forEach(index => {
-                const options = <MongodbIndexOptions>{ name: index.name, unique: index.isUnique, sparse: index.isSparse, background: index.isBackground, expireAfterSeconds: index.expireAfterSeconds };
+                const options: MongodbIndexOptions = Object.assign({}, {
+                    name: index.name,
+                    unique: index.isUnique,
+                    sparse: index.isSparse,
+                    background: index.isBackground
+                }, index.expireAfterSeconds === undefined
+                    ? {}
+                    : { expireAfterSeconds: index.expireAfterSeconds });
                 promises.push(queryRunner.createCollectionIndex(metadata.tableName, index.columnNamesWithOrderingMap, options));
             });
         });
--- test/functional/mongodb/basic/mongo-index/entity/Post.ts
+++ test/functional/mongodb/basic/mongo-index/entity/Post.ts
@@ -10,6 +10,7 @@ import {ObjectID} from "../../../../../../src/driver/mongodb/typings";
 @Index("title_name_count", () => ({ title: 1, name: 1, count: 1 }))
 @Index("title_name_count_reversed", () => ({ title: -1, name: -1, count: -1 }))
 @Index("count_in_background", () => ({ count: -1 }), {background: true})
+@Index("count_expire", () => ({ title: -1 }), {expireAfterSeconds: 3600})
 export class Post {
 
     @ObjectIdColumn()
