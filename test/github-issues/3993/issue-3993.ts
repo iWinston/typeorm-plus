d771723bfd91485760b0f4f4e31668f41ca2cb12
YehezkielSaputra <yehezkiel.saputra@hotmail.com>
2019-04-25 14:42:31 +0800

    Create test file for issue 3993

---
+++ test/github-issues/3993/issue-3993.ts
@@ -0,0 +1,34 @@
+import "reflect-metadata";
+import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {Connection} from "../../../src/connection/Connection";
+import { TableColumn } from "../../../src/schema-builder/table/TableColumn";
+import { Table } from "../../../src/schema-builder/table/Table";
+
+describe("github issues > #3993 Rename default table used in query caching", () => {
+
+    let connections: Connection[];
+    before(async () => connections = await createTestingConnections({
+        enabledDrivers: ["postgres"],
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        schemaCreate: true,
+        dropSchema: true,
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    after(() => closeTestingConnections(connections));
+
+    it("Should create table with generated column", () => Promise.all(connections.map(async connection => {
+        const id = new TableColumn({
+            name: "id",
+            type: "uuid",
+            generationStrategy: "uuid",
+            isGenerated: true,
+            isPrimary: true
+        });
+        const client = new Table({
+            name: "table",
+            columns: [id]
+        });
+        await connection.createQueryRunner().createTable(client);
+    })));
+
+});
