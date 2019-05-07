18e2292ce6d9820e83d6f10dd4a7885ac4adc9b6
Luis Emilio Velasco Sánchez <emibloque@gmail.com>
2019-05-07 16:50:27 +0200

    Remove try and catch within test

--- test/github-issues/4096/issue-4096.ts
+++ test/github-issues/4096/issue-4096.ts
@@ -19,43 +19,39 @@ describe("github issues > #4096 SQLite support for orUpdate", () => {
     after(() => closeTestingConnections(connections));
 
     it("should overwrite using current value in SQLite", () => Promise.all(connections.map(async connection => {
-      try {
-        const user1 = new User();
-        user1.email = "example@example.org";
-        user1.username = "example";
-        user1.bio = "My bio";
+      const user1 = new User();
+      user1.email = "example@example.org";
+      user1.username = "example";
+      user1.bio = "My bio";
 
-        const user2 = new User();
-        user2.email = "example@example.org";
-        user2.username = "example";
-        user2.bio = "Updated bio";
+      const user2 = new User();
+      user2.email = "example@example.org";
+      user2.username = "example";
+      user2.bio = "Updated bio";
 
-        const UserRepository = connection.manager.getRepository(User);
+      const UserRepository = connection.manager.getRepository(User);
 
-        await UserRepository
-          .createQueryBuilder()
-          .insert()
-          .into(User)
-          .values(user1)
-          .execute();
+      await UserRepository
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values(user1)
+        .execute();
 
-        await UserRepository
-          .createQueryBuilder()
-          .insert()
-          .into(User)
-          .values(user2)
-          .orUpdate({
-            conflict_target: [ "email", "username" ],
-            overwrite: ["bio"],
-          })
-          .execute();
+      await UserRepository
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values(user2)
+        .orUpdate({
+          conflict_target: [ "email", "username" ],
+          overwrite: ["bio"],
+        })
+        .execute();
 
-        const users = await UserRepository.find();
-        expect(users).not.to.be.undefined;
-        expect(users).to.have.lengthOf(1);
-        expect(users[0]).to.includes({ bio: "Updated bio" });
-      } catch (err) {
-        throw new Error(err);
-      }
+      const users = await UserRepository.find();
+      expect(users).not.to.be.undefined;
+      expect(users).to.have.lengthOf(1);
+      expect(users[0]).to.includes({ bio: "Updated bio" });
     })));
  });
