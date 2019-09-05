7a0beedc365ab5090d08e1482abcdbc45e558933
Mophy Xiong <mophy.xh@gmail.com>
2019-09-06 03:33:39 +0800

    fix: the excessive stack depth comparing types `FindConditions<?>` and `FindConditions<?>` problem (#4470)

--- src/find-options/FindConditions.ts
+++ src/find-options/FindConditions.ts
@@ -4,5 +4,5 @@ import {FindOperator} from "./FindOperator";
  * Used for find operations.
  */
 export type FindConditions<T> = {
-    [P in keyof T]?: FindConditions<T[P]>|FindOperator<FindConditions<T[P]>>;
+    [P in keyof T]?: T[P] extends never ? FindConditions<T[P]>|FindOperator<FindConditions<T[P]>> : FindConditions<T[P]>|FindOperator<FindConditions<T[P]>>;
 };
