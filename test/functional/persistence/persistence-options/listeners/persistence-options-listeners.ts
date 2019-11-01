40258d3422e4b9b8ddf241124695bbb357a90afa
winston <iwinston@163.com>
2019-11-01 17:31:40 +0800

    added test: support of the cascades soft-remove and recover

--- test/functional/persistence/persistence-options/listeners/persistence-options-listeners.ts
+++ test/functional/persistence/persistence-options/listeners/persistence-options-listeners.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../../utils/test-utils";
 import {Post} from "./entity/Post";
 import {Connection} from "../../../../../src/connection/Connection";
+import {PostWithDeleteDateColumn} from "./entity/PostWithDeleteDateColumn";
 // import {expect} from "chai";
 
 describe("persistence > persistence options > listeners", () => {
@@ -51,6 +52,26 @@ describe("persistence > persistence options > listeners", () => {
         await connection.manager.save(post);
         await connection.manager.remove(post, { listeners: false });
         post.isRemoved.should.be.equal(false);
+    })));
+
+    it("soft-remove listeners should work by default", () => Promise.all(connections.map(async connection => {
+        const post = new PostWithDeleteDateColumn();
+        post.title = "Bakhrom";
+        post.description = "Hello";
+        await connection.manager.save(post);
+        await connection.manager.softRemove(post);
+        post.title.should.be.equal("Bakhrom!");
+        post.isSoftRemoved.should.be.equal(true);
+    })));
+
+    it("soft-remove listeners should be disabled if remove option is specified", () => Promise.all(connections.map(async connection => {
+        const post = new PostWithDeleteDateColumn();
+        post.title = "Bakhrom";
+        post.description = "Hello";
+        await connection.manager.save(post);
+        await connection.manager.softRemove(post, { listeners: false });
+        post.title.should.be.equal("Bakhrom");
+        post.isSoftRemoved.should.be.equal(false);
     })));
 
 });
