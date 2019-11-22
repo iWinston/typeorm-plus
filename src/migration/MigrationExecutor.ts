c0c8566d6fde6c3a794798d4dc9c27f20cbac762
Gerwin Brunner <gerwin.brunner@gmail.com>
2019-11-22 04:38:06 +0100

    fix: unescaped column name in order clause of "migrations" (#5108)
    * Fixed unescaped column name in order clause of "migrations"
    
    Fix for https://github.com/typeorm/typeorm/issues/5102
    
    * made it driver independent

--- src/migration/MigrationExecutor.ts
+++ src/migration/MigrationExecutor.ts
@@ -389,7 +389,7 @@ export class MigrationExecutor {
             const migrationsRaw: ObjectLiteral[] = await this.connection.manager
             .createQueryBuilder(queryRunner)
             .select()
-            .orderBy("id", "DESC")
+            .orderBy(this.connection.driver.escape("id"), "DESC")
             .from(this.migrationsTable, this.migrationsTableName)
             .getRawMany();
             return migrationsRaw.map(migrationRaw => {
