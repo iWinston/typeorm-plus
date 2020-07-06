0d99b4662518c8489e9aeba9d87a8028e16c3899
yokomotod <yokomotod@gmail.com>
2020-07-07 07:37:13 +0900

    fix: remove unnecessary optionality from Raw operator's columnAlias argument (#6321)

--- src/find-options/operator/Raw.ts
+++ src/find-options/operator/Raw.ts
@@ -4,6 +4,6 @@ import {FindOperator} from "../FindOperator";
  * Find Options Operator.
  * Example: { someField: Raw([...]) }
  */
-export function Raw<T>(value: string|((columnAlias?: string) => string)) {
+export function Raw<T>(value: string|((columnAlias: string) => string)) {
     return new FindOperator("raw", value as any, false);
 }
